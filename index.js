8e8157eecc7ac452e1e662e1ff8547e57306fc65
Allen <allenshamrock37@gmail.com>
2024-01-18 20:35:43 +0300

    Implemented the toggle between forms feature

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-function toggleForm() {
-  const forms= document.querySelectorAll(".patient-form-container");
- 
+let currentForm = null;
+//Toogle function to access the forms
+function toggleForm(formId) {
+  const form = document.getElementById(formId);
+  //Conditional statement to show only clicked form
+  if (currentForm) {
+    currentForm.style.display = "none";
+  }
+
+  if (form) {
+    form.style.display = "flex";
+    currentForm = form;
+  }
+}
+const url = "http://localhost:3000/data";
+const reqAppointment = document.getElementById("reqAppointment");
+function fetchData() {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 }
 
-toggleForm();
+fetchData();
+
+//using POST method to post patients data to db server
+
+
